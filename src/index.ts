8fa910d66a740f689474e9ebe5f11005c949da6e
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-09-01 15:48:08 +0530

    checking workflow

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
+app.get("/working", (req, res) => {
+  res.send("Hello World working!")
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
 })
